35e93cc6a86c4fed99a19b4e5665cbe468ab8923
Lahiru Sandaruwan <118237955+LahiruLuci@users.noreply.github.com>
2025-06-14 11:50:56 +0530

    build home screen

--- App.js
+++ App.js
@@ -1,15 +1,42 @@
+import { LinearGradient } from 'expo-linear-gradient';
 import './global.css';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image, TouchableOpacity } from 'react-native';
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+
 
 export default function App() {
   return (
-    <View className="flex-1 bg-blue-500 justify-center items-center">
-      <Text className="text-white text-2xl font-bold">
-        Hello NativeWind with Expo!
-      </Text>
-      <StatusBar style="auto" />
+    <View className="flex-1 flex justify-end">
+      <StatusBar style='light'/>
+      <Image className="h-full w-full absolute" source={require('./assets/images/welcome.png')}/>
+      <LinearGradient
+        colors={['transparent', '#18181b']}
+        style={{width: wp(100), height: hp(100)}}
+        start={{x: 0.5, y: 0}}
+        end={{x: 0.5, y: 0.8}}
+        className="flex justify-end pb-12 space-y-8"
+      >
+        <View className="flex items-center">
+          <Text style={{fontSize: hp(5)}} className="text-white font-bold text-center tracking-wide">
+            Best <Text className="text-rose-500">Workouts</Text>
+          </Text>
+          <Text style={{fontSize: hp(5)}} className="text-white font-bold text-center tracking-wide">
+            For  <Text className="text-rose-500">you</Text>
+          </Text>
+        </View>
+        <View>
+          <TouchableOpacity
+            style={{height: hp(7), width: wp(80)}}
+            className="bg-rose-500 rounded-full flex items-center justify-center mx-auto border-[2px] border-neutral-200"
+          >
+            <Text style={{fontSize: hp(3)}} className="text-white font-bold tracking-widest">
+              Get Started
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </LinearGradient>
     </View>
   );
 }
