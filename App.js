18e17a079f7cff13779f71580086cf3020ac392c
Lahiru Sandaruwan <118237955+LahiruLuci@users.noreply.github.com>
2025-06-14 13:01:49 +0530

    Add some animation to the home page

--- App.js
+++ App.js
@@ -4,9 +4,11 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Text, View, Image, TouchableOpacity } from 'react-native';
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
-
+import Animated, {FadeIn, FadeOut, FadeInDown} from 'react-native-reanimated';
+//import {useRouter} from 'expo-router';
 
 export default function App() {
+  const router = useRouter();
   return (
     <View className="flex-1 flex justify-end">
       <StatusBar style='light'/>
@@ -18,24 +20,26 @@ export default function App() {
         end={{x: 0.5, y: 0.8}}
         className="flex justify-end pb-12 space-y-8"
       >
-        <View className="flex items-center">
+        <Animated.View entering={FadeInDown.delay(100).springify()} className="flex items-center">
           <Text style={{fontSize: hp(5)}} className="text-white font-bold text-center tracking-wide">
             Best <Text className="text-rose-500">Workouts</Text>
           </Text>
           <Text style={{fontSize: hp(5)}} className="text-white font-bold text-center tracking-wide">
             For  <Text className="text-rose-500">you</Text>
           </Text>
-        </View>
-        <View>
+        </Animated.View>
+
+        <Animated.View entering={FadeInDown.delay(200).springify()} >
           <TouchableOpacity
+            //onPress={() => router.push('home')}
             style={{height: hp(7), width: wp(80)}}
             className="bg-rose-500 rounded-full flex items-center justify-center mx-auto border-[2px] border-neutral-200"
           >
             <Text style={{fontSize: hp(3)}} className="text-white font-bold tracking-widest">
               Get Started
-            </Text>
+            </Text> 
           </TouchableOpacity>
-        </View>
+        </Animated.View>
       </LinearGradient>
     </View>
   );
