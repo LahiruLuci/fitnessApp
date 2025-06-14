e2b7d201d2eee09b538ee5854e15e2cc6de8a207
Lahiru Sandaruwan <118237955+LahiruLuci@users.noreply.github.com>
2025-06-14 10:38:27 +0530

    first commit✌

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
+import './global.css';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className="flex-1 bg-blue-500 justify-center items-center">
+      <Text className="text-white text-2xl font-bold">
+        Hello NativeWind with Expo!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
